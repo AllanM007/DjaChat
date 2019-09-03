fae1a76551d6acbf62f7850489fc55e0e573194c
Allan Muiruri <Allan Muiruri>
2019-09-03 23:48:28 +0300

    v1.0

---
+++ chatbot/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+from chatterbot import ChatBot
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+bot = ChatBot(
+    'Ron Obvious',
+    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+    )
+
+chatbot.train("chatbot.corpus.english")
+
+@csrf_exempt
+
+def get_response(request):
+    response = {'status':None}
+    
+    if request.method == 'POST':
+        data.json.loads(request.body)
+        message = data["message"]
+        
+        chat_response = chat.get_response(message).text
+        response['message'] = {'text': chat_response, 'user' : False, 'chat_bot' : True}
+        response['status'] = 'ok'
+    else:
+        response['error'] = 'no post data found'
+
+    return HttpResponse(
+        json.dumps(response),
+        content_type = "application/json"
+    )
+def home(request, template_name="home.html"):
+    context = {"title":'Chatbot Version 1.0'}
+    return render_to_response(template_name,context)
